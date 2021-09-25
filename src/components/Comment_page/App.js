9114e7369a1e0124c79bcc1099b6b1f2031feac5
Jesslyn <jesslynwjx@gmail.com>
2021-09-25 22:35:27 +0800

    comment page

---
+++ src/components/Comment_page/App.js
@@ -0,0 +1,12 @@
+import MainPage from './main_page'
+
+
+function App() {
+  return (
+    <div className="App">
+        <MainPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Comment_page/Comments/index.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import PubSub from 'pubsub-js'
+import {nanoid} from 'nanoid'
+import { Input } from 'antd';
+const { TextArea } = Input;
+
+
+export default class Comments extends Component {
+
+    addComments = () =>{
+        const content = this.comment.value
+        console.log(content);
+        const comment = {id:nanoid(),content}
+        //发布消息
+        PubSub.publish('comments', comment)
+    }
+
+    render() {
+        // const onChange = e => {
+        //     if 
+        //     console.log('Change:', e.target.value);
+        // };
+
+        return (
+            <div>
+                <div>
+                    <TextArea ref = {c => this.comment = c} type="text"  showCount maxLength={100} onChange={onChange} />
+                    {/* <input ref = {c => this.comment = c}type="text" /> */}
+                    <button onClick = {this.addComments}>send</button>
+                </div>
+            </div>
+        )
+    }
+}
