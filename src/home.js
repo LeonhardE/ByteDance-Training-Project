76247c05f26253f31ed5d903de060fa2bda3d474
noon000 <764020793@qq.com>
2021-09-25 09:46:21 +0800

    feat:homepage

--- src/home.js
+++ src/home.js
@@ -1,41 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import './style/css/homepage.css';
-import {Link} from "react-router-dom";
+import "./style/css/homepage.css";
+import { Link } from "react-router-dom";
 export default function HomePage() {
+  const editorStyle = {
+    width: "100vw",
+    height: "100vh",
+    border: "0",
+    overflow: "hidden",
+  };
 
-    const editorStyle = {
-        width: "100vw",
-        height: "100vh",
-        border: '0',
-        overflow: "hidden"
-    }
-
-    return (
-        <div>
-            <div className='navTop'>
-                <div className='butt'></div>
-            </div>
-            <div className='flexContainer'>
-                <div className='leftButton'>
-                    <Link to='/nav' className='butt'></Link>
-                </div>
-
-
-                <iframe
-                    id='editor'
-                    src="https://codesandbox.io/embed/pedantic-microservice-z5vxg?fontsize=14&hidenavigation=1&theme=dark&view=editor"
-                    style={editorStyle}
-                    title="pedantic-microservice-z5vxg"
-                    allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-                    sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-                ></iframe>
-            </div>
-            <div className='navBottom'>
-                <div className='butt'></div>
-            </div>
+  return (
+    <div>
+      <div className="navTop">
+        <div className="butt"></div>
+      </div>
+      <div className="flexContainer">
+        <div className="leftButton">
+          <Link to="/nav" className="butt"></Link>
         </div>
-      </Drawer>
+
+        <iframe
+          id="editor"
+          src="https://codesandbox.io/embed/pedantic-microservice-z5vxg?fontsize=14&hidenavigation=1&theme=dark&view=editor"
+          style={editorStyle}
+          title="pedantic-microservice-z5vxg"
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        ></iframe>
+      </div>
+      <div className="navBottom">
+        <div className="butt"></div>
+      </div>
     </div>
   );
 }
