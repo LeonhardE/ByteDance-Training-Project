25bd6fc0d5b70101d022c34b7ac84e18423fee3a
Colin3191 <319151881@qq.com>
2021-09-25 22:12:49 +0800

    fix:修改文件名

--- src/components/ResourcePage/data/knowledge.js
+++ src/components/ResourcePage/data/knowledge.js
@@ -1,14 +1,14 @@
 export const knowledge = [
-  {
-    title: 'website1',
-    addr: 'https://www.baidu.com'
-  },
-  {
-    title: 'website2',
-    addr: 'https://www.baidu.com'
-  },
-  {
-    title: 'website3',
-    addr: 'https://www.baidu.com'
-  }
-]
+    {
+      title: 'website1',
+      addr: 'https://www.baidu.com'
+    },
+    {
+      title: 'website2',
+      addr: 'https://www.baidu.com'
+    },
+    {
+      title: 'website3',
+      addr: 'https://www.baidu.com'
+    }
+  ]
