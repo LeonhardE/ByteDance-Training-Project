95e2663b0f4d7da76e62f48d4468ac3882d41178
noon000 <764020793@qq.com>
2021-09-25 20:54:46 +0800

    fix reflicts

--- src/home.js
+++ src/home.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import 'antd/dist/antd.css'
+
 import './style/css/homepage.css';
-import {
-    Link
-} from "react-router-dom";
+import {Link} from "react-router-dom";
 export default function HomePage() {
 
     const editorStyle = {
-        width: "100%",
-        minHeight: "825px",
+        width: "100vw",
+        height: "100vh",
         border: '0',
-        borderRadius: "4px",
         overflow: "hidden"
     }
 
@@ -24,16 +21,16 @@ export default function HomePage() {
                     <Link to='/nav' className='butt'></Link>
                 </div>
 
-             
-               <iframe
+
+                <iframe
                     id='editor'
                     src="https://codesandbox.io/embed/pedantic-microservice-z5vxg?fontsize=14&hidenavigation=1&theme=dark&view=editor"
                     style={editorStyle}
                     title="pedantic-microservice-z5vxg"
                     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
                     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-                ></iframe> 
-                
+                ></iframe>
+
             </div>
             <div className='navBottom'>
                 <div className='butt'></div>
