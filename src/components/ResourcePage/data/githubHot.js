fb09bfe494dcdd7b0d9875564f453617fcc14475
CassyYu <980317866@qq.com>
2021-09-26 08:56:18 +0800

    third commit ResourcePage

--- src/components/ResourcePage/data/githubHot.js
+++ src/components/ResourcePage/data/githubHot.js
@@ -12,5 +12,103 @@ export const githubHot = [
       '@DefTruth'
     ],
     rankDesc: '683 stars today'
+  },
+  {
+    addr: 'https://github.com/angular/angular-cli',
+    owner: 'PeterL1n',
+    name: 'RobustVideoMatting',
+    desc: 'Robust Video Matting in PyTorch, TensorFlow, TensorFlow.js, ONNX, CoreML!',
+    lang: 'Python',
+    stars: 2442,
+    forks: 219,
+    builder: [
+      '@PeterL1n',
+      '@DefTruth'
+    ],
+    rankDesc: '683 stars today'
+  },
+  {
+    addr: 'https://github.com/angular/angular-cli',
+    owner: 'PeterL1n',
+    name: 'RobustVideoMatting',
+    desc: 'Robust Video Matting in PyTorch, TensorFlow, TensorFlow.js, ONNX, CoreML!',
+    lang: 'Python',
+    stars: 2442,
+    forks: 219,
+    builder: [
+      '@PeterL1n',
+      '@DefTruth'
+    ],
+    rankDesc: '683 stars today'
+  },
+  {
+    addr: 'https://github.com/angular/angular-cli',
+    owner: 'PeterL1n',
+    name: 'RobustVideoMatting',
+    desc: 'Robust Video Matting in PyTorch, TensorFlow, TensorFlow.js, ONNX, CoreML!',
+    lang: 'Python',
+    stars: 2442,
+    forks: 219,
+    builder: [
+      '@PeterL1n',
+      '@DefTruth'
+    ],
+    rankDesc: '683 stars today'
+  },
+  {
+    addr: 'https://github.com/angular/angular-cli',
+    owner: 'PeterL1n',
+    name: 'RobustVideoMatting',
+    desc: 'Robust Video Matting in PyTorch, TensorFlow, TensorFlow.js, ONNX, CoreML!',
+    lang: 'Python',
+    stars: 2442,
+    forks: 219,
+    builder: [
+      '@PeterL1n',
+      '@DefTruth'
+    ],
+    rankDesc: '683 stars today'
+  },
+  {
+    addr: 'https://github.com/angular/angular-cli',
+    owner: 'PeterL1n',
+    name: 'RobustVideoMatting',
+    desc: 'Robust Video Matting in PyTorch, TensorFlow, TensorFlow.js, ONNX, CoreML!',
+    lang: 'Python',
+    stars: 2442,
+    forks: 219,
+    builder: [
+      '@PeterL1n',
+      '@DefTruth'
+    ],
+    rankDesc: '683 stars today'
+  },
+  {
+    addr: 'https://github.com/angular/angular-cli',
+    owner: 'PeterL1n',
+    name: 'RobustVideoMatting',
+    desc: 'Robust Video Matting in PyTorch, TensorFlow, TensorFlow.js, ONNX, CoreML!',
+    lang: 'Python',
+    stars: 2442,
+    forks: 219,
+    builder: [
+      '@PeterL1n',
+      '@DefTruth'
+    ],
+    rankDesc: '683 stars today'
+  },
+  {
+    addr: 'https://github.com/angular/angular-cli',
+    owner: 'PeterL1n',
+    name: 'RobustVideoMatting',
+    desc: 'Robust Video Matting in PyTorch, TensorFlow, TensorFlow.js, ONNX, CoreML!',
+    lang: 'Python',
+    stars: 2442,
+    forks: 219,
+    builder: [
+      '@PeterL1n',
+      '@DefTruth'
+    ],
+    rankDesc: '683 stars today'
   }
 ]
