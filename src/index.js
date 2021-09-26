028ede57570351810048cbeb1fbd1f59c741d4d9
LeonhardE <31626796+LeonhardE@users.noreply.github.com>
2021-09-26 10:31:31 +0800

    combine with main

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import HomePage from './home';
-import Friend from './components/Friend/friend'
-// import App from './App'
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from 'react-router-dom'
+import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    {/* <HomePage /> */}
-    <Friend />
-    {/* <App /> */}
-  </React.StrictMode>,
+  <Router>
+    <App/>
+  </Router>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
