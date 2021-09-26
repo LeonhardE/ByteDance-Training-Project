90cdb3fb726f3f9937e413f7626813717e2c99c0
MagicMooc <riyingxiaozi@gmail.com>
2021-09-26 13:32:26 +0800

    Update websites.js

--- src/components/ResourcePage/data/websites.js
+++ src/components/ResourcePage/data/websites.js
@@ -14,6 +14,70 @@ export const websites = [
         title: "Moodjoy",
         addr: "https://moodjoy.com/",
       },
+      {
+        title: "墨刀",
+        addr: "https://modao.cc/",
+      },
+      {
+        title: "在线PS",
+        addr: "https://www.photopea.com/",
+      },
+      {
+        title: "Github短网址",
+        addr: "https://git.io/",
+      },
+      {
+        title: "短链生成",
+        addr: "http://www.dh6.ink/",
+      },
+      {
+        title: "JSON格式化",
+        addr: "http://www.bejson.com/",
+      },
+      {
+        title: "草料二维码",
+        addr: "https://cli.im/",
+      },
+      {
+        title: "草料二维码",
+        addr: "https://cli.im/",
+      },
+      {
+        title: "Regex101",
+        addr: "https://regex101.com/",
+      },
+      {
+        title: "Regexr",
+        addr: "https://regexr.com/",
+      },
+      {
+        title: "Aconvert",
+        addr: "https://www.aconvert.com/cn/",
+      },
+      {
+        title: "EmoijBuilder",
+        addr: "http://phlntn.com/emojibuilder/",
+      },
+      {
+        title: "CanIUse",
+        addr: "https://caniuse.com/",
+      },
+      {
+        title: "图片在线裁剪",
+        addr: "https://www.asqql.com/gifc/",
+      },
+      {
+        title: "haoip",
+        addr: "https://haoip.cn/",
+      },
+      {
+        title: "squoosh",
+        addr: "https://squoosh.net/",
+      },
+      {
+        title: "tinypng",
+        addr: "https://tinypng.com/",
+      },
     ],
   },
   {
@@ -38,6 +102,38 @@ export const websites = [
       {
         title: "Pinterest",
         addr: "https://www.pinterest.com/",
+      },
+      {
+        title: "站酷",
+        addr: "https://www.zcool.com.cn/",
+      },
+      {
+        title: "FWA",
+        addr: "https://thefwa.com/",
+      },
+      {
+        title: "Lapa",
+        addr: "https://www.lapa.ninja/",
+      },
+      {
+        title: "UIMovement",
+        addr: "https://uimovement.com/",
+      },
+      {
+        title: "UI中国",
+        addr: "https://www.ui.cn/",
+      },
+      {
+        title: "LandBook",
+        addr: "https://land-book.com/",
+      },
+      {
+        title: "PSDRepo",
+        addr: "https://psdrepo.com/",
+      },
+      {
+        title: "BestWebsite",
+        addr: "https://bestwebsite.gallery/",
       },
     ],
   },
